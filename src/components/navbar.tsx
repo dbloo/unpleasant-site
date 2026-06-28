a6e9aeb53b872dbd859f70978284ae2e982a4df6
Dominic Bloomfield <nooboon14@gmail.com>
2026-06-28 17:41:45 -0400

    updates

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,25 @@
-import {ShoppingCart} from 'lucide-react'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import {motion} from 'framer-motion'
 
 
 function Navbar() { 
+    
 
     const [isOpen, setIsOpen] = useState(false);
+
+
     return (
         <div className="w-screen  relative">
-        <motion.nav 
+        <motion.nav style={{ paddingTop: 'env(safe-area-inset-top)' }}
         
         initial={{ y: -720, opacity: 1 }}
-    animate={isOpen ? { y:-500, opacity: 1, transition: {type:"spring", stiffness:1000, damping: 100, duration:0.5, ease: "easeInOut"} } :  { y: -720, opacity: 1 }}
+    animate={isOpen ? { y:-500, opacity: 1, transition: {type:"spring", stiffness:1000, damping: 100, duration:0.5, ease: "easeInOut"} } :  { y: -700, opacity: 1 }}
 
-        className="h-200 z-1000  fixed  w-full bg-background text-black ">
+        className="h-190  z-1000  fixed  w-full bg-background text-black ">
             <div className=" lg:px-10  absolute bottom-0 right-0 flex flex-row w-full items-center justify-between ">   
                 <div className=' flex  h-full w-full flex-row items-center justify-between'>
                 <a onClick={() => setIsOpen(false)} href = "/"><img src = "/assets/logo.svg" className="ml-5 lg:size-30 lg:pt-10 pt-5 size-25"></img></a>
-                <div className= {`lg:pt-10 lg:relative absolute lg:bottom-0 lg:left-0  lg:text-lg text-2xl bottom-20  flex-col ${isOpen ? "":""} lg:flex-row flex lg:w-1/6 w-screen text-center  items-center gap-4`}>
+                <div className= {`lg:pt-10 lg:relative absolute lg:bottom-0 lg:left-0  lg:text-lg text-2xl bottom-20 bg-background flex-col ${isOpen ? "":""} lg:flex-row flex lg:w-1/6 w-screen text-center  items-center gap-4`}>
                     <a onClick={() => setIsOpen(false)} href="/about" className="px-3 py-2 border-b w-full lg:border-0 border-black hover:text-amber-500 ">About</a>
                     {/* <a onClick={() => setIsOpen(false)} href="/lookbook" className="px-3 py-2 hover:text-amber-500 rounded">Lookbook</a> */}
                     <a onClick={() => setIsOpen(false)} href="/contact" className="px-3 py-2 border-b  w-full lg:border-0 border-black hover:text-amber-500 ">Contact</a>
@@ -28,7 +30,7 @@ function Navbar() {
                     
                 </div>
                 
-                <div className = " mr-5 pt-5 items-center flex flex-row"> 
+                <div className = " mr-5 pt-5 items-center flex flex-row " > 
                     {/* <ShoppingCart size={35}/>                     */}
                 <div className="flex gap-1 flex-col lg:hidden cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
                 <motion.span
@@ -54,7 +56,7 @@ function Navbar() {
                                        
             </div>
         </motion.nav>
-        <motion.div initial = {{opacity: 0}} animate= {isOpen ? {opacity: 0.25} : {opacity: 0 }}className={` ${isOpen ? "": " hidden"}w-screen opacity-25 h-screen fixed bg-black z-10`}></motion.div>
+        <motion.div initial = {{opacity: 0}} animate= {isOpen ? {opacity: 0.25} : {opacity: 0 }}className={` w-screen opacity-25 h-screen fixed bg-black z-10`}></motion.div>
         </div>
     );
 }
